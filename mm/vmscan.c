8c86284fc3daaa3521ae692f9c8287d574bb4dfa
LeanxModulostk <eduardo.esmvc@gmail.com>
2026-01-17 16:02:39 -0500

    Revert "FROMGIT: BACKPORT: Multi-gen LRU: Avoid race in inc_min_seq()"
    This reverts commit 771e220f2770920d2fab7dac7f597d80862e9a00.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3888,7 +3888,7 @@ static void inc_max_seq(struct lruvec *lruvec, bool can_swap, bool full_scan)
 	int type, zone;
 	struct lru_gen_struct *lrugen = &lruvec->lrugen;
 	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
-restart:
+
 	spin_lock_irq(&pgdat->lru_lock);
 
 	VM_WARN_ON_ONCE(!seq_is_valid(lruvec));
@@ -3899,12 +3899,11 @@ restart:
 
 		VM_WARN_ON_ONCE(!full_scan && (type == LRU_GEN_FILE || can_swap));
 
-		if (inc_min_seq(lruvec, type, can_swap))
-			continue;
-
-		spin_unlock_irq(&pgdat->lru_lock);
-		cond_resched();
-		goto restart;
+		while (!inc_min_seq(lruvec, type, can_swap)) {
+			spin_unlock_irq(&pgdat->lru_lock);
+			cond_resched();
+			spin_lock_irq(&pgdat->lru_lock);
+		}
 	}
 
 	/*
