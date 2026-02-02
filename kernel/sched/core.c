8d6fec14f4bb490b8e738da65b5fb42bbb09503c
LeanxModulostk <eduardo.esmvc@gmail.com>
2026-02-01 20:20:39 -0500

    Revert "sched: core: Add another heavy thread of CODM"
    This reverts commit 1179ccb19cbd96f9790510e5771e5b1a69888db4.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6320,7 +6320,7 @@ static bool task_is_unity_game(struct task_struct *p)
 		rcu_read_lock();
 		for_each_thread(p, t) {
 			/* Check for a UnityMain thread in the thread group */
-			if (!strcmp(t->comm, "UnityMain") || !strcmp(t->comm, "UnityGfxDeviceW")) {
+			if (!strcmp(t->comm, "UnityMain")) {
 				ret = true;
 				break;
 			}
